2782ae4647ad47ba49588db9d0826ce54b77d01a
rahin21 <mdrahinzaman21@gmail.com>
2023-10-16 23:32:02 +0600

    toggle tab fixed

--- app/components/about/toggleTab.tsx
+++ app/components/about/toggleTab.tsx
@@ -1,12 +1,19 @@
-import React from "react";
 
-const ToggleTab = () => {
+
+
+
+const ToggleTab = (props: any) => {
+  
+
   return (
-    <div className="flex justify-center mt-32 font-nunito text-3xl">
-      <div className="flex justify-between items-center rounded-full w-[60rem] bg-[#151934] h-[5.5625rem] px-16">
-        <h3 className="bg-[#7214ffe2] w-[16rem] h-[4rem] flex justify-center items-center rounded-full">#1 Discovery</h3>
-        <h3 className="bg-[#7214ffe2] w-[16rem] h-[4rem] flex justify-center items-center rounded-full">#2 Design</h3>
-        <h3 className="bg-[#7214ffe2] w-[16rem] h-[4rem] flex justify-center items-center rounded-full">#3 Development</h3>
+    <div className="flex justify-center mt-32 font-nunito text-2xl">
+      <div className="bg-gradient-to-br from-indigo-900/[0.09] via-violet-900/[0.1] to-purple-900/[0.3] flex justify-between items-center rounded-full w-[50rem] bg-[#151934] h-[5rem] px-8">
+        {props.aboutLists.map((aboutList:string, i:number) => (
+        
+        <h3 key={i} onClick={() => props.setSelected(i)} className={`${props.Selected == i? "bg-[#7214ffe2]": ""} transition duration-700 cursor-default w-[15rem] h-[4rem] flex justify-center items-center rounded-full`}>
+          {aboutList}
+        </h3>
+        ))}
       </div>
     </div>
   );
