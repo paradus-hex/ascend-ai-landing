2a727825e4646e92d16eff37c2cc7ff22f83248f
Asif Rahman <57532856+asifrahman18@users.noreply.github.com>
2023-10-21 17:05:10 +0600

    updates

--- components/nav.tsx
+++ components/nav.tsx
@@ -16,7 +16,7 @@ export default function Nav() {
   ]
 
   return (
-    <nav className="w-full border-b md:border-0 font-nunito">
+    <nav className="w-full border-b md:border-0 font-nunito fixed backdrop-blur-md backdrop-filter bg-opacity-50">
       <div className="items-center px-4 max-w-screen-xl mx-auto md:flex md:px-8">
         <div className="flex items-center justify-between py-3 md:py-5 md:block">
           <Link href="/">
